beff797ac2f86e89df716d4dfa86198fba3420f7
Will Bailey <78081479+wb314cam@users.noreply.github.com>
2021-04-26 13:49:24 +0100

    Added extra channels

--- Assets/Scripts/PlottingController.cs
+++ Assets/Scripts/PlottingController.cs
@@ -8,8 +8,15 @@ public class PlottingController : MonoBehaviour
 {
     private Renderer renderer;
     private float[] testSignal;
+    public float testSignalFrequency = 10f;
+    public float testSignalSpeed = 1f;
     [NonSerialized]
-    public float[] signal;
+    public float[] signal_0;
+    [NonSerialized] 
+    public float[] signal_1;
+    [NonSerialized] 
+    public float[] signal_2;
+
     private int signalIndex;
     public bool useTestSignal;
 
@@ -24,22 +31,77 @@ public class PlottingController : MonoBehaviour
         if (signalIndex >= 1000) signalIndex -= 1000;
     }
 
-
-    public void UpdateSignal(float x)
+    public void IncrementSignalIndex(int x)
     {
-        signal[signalIndex] = x;
         signalIndex++;
         if (signalIndex >= 1000) signalIndex -= 1000;
     }
 
-    public void UpdateSignal(float[] x)
+    public void DecrementSignalIndex(int x)
     {
-        for (int i = 0; i < x.Length; i++)
+        signalIndex--;
+        if (signalIndex <= 0) signalIndex += 1000;
+    }
+
+    public void UpdateSignal(float x, int channel)
+    {
+        switch (channel)
         {
-            signal[signalIndex] = x[i];
-            signalIndex++;
-            if (signalIndex >= 1000) signalIndex -= 1000;
+            case 0:
+                signal_0[signalIndex] = x;
+                break;
+
+            case 1:
+                signal_1[signalIndex] = x;
+                break;
+
+            case 2:
+                signal_2[signalIndex] = x;
+                break;
+            default:
+                signal_0[signalIndex] = x;
+                break;
+
         }
+    }
+
+    public void UpdateSignal(float[] x, int channel)
+    {
+        switch (channel)
+        {
+            case 0:
+                for (int i = 0; i < x.Length; i++)
+                {
+                    signal_0[signalIndex] = x[i];
+                    
+                }
+                break;
+
+            case 1:
+                for (int i = 0; i < x.Length; i++)
+                {
+                    signal_1[signalIndex] = x[i];
+                    
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < x.Length; i++)
+                {
+                    signal_2[signalIndex] = x[i];
+                    
+                }
+                break;
+            default:
+                for (int i = 0; i < x.Length; i++)
+                {
+                    signal_0[signalIndex] = x[i];
+                    
+                }
+                break;
+
+        }
+        
     }
 
     // Start is called before the first frame update
@@ -62,13 +124,17 @@ public class PlottingController : MonoBehaviour
         {
             for (int i = 0; i < 1000; i++)
             {
-                testSignal[i] = Mathf.Sin(((float)i * 6.28f) / 500f + (Time.time));
+                testSignal[i] = Mathf.Sin(((float)i * 6.28f*testSignalFrequency) + (Time.time*testSignalSpeed));
             }
-            renderer.material.SetFloatArray("_data", testSignal);
+            renderer.material.SetFloatArray("_data_ch1", testSignal);
+            renderer.material.SetFloatArray("_data_ch2", testSignal);
+            renderer.material.SetFloatArray("_data_ch3", testSignal);
         }
         else
         {
-            renderer.material.SetFloatArray("_data", signal);
+            renderer.material.SetFloatArray("_data_ch1", signal_0);
+            renderer.material.SetFloatArray("_data_ch2", signal_1);
+            renderer.material.SetFloatArray("_data_ch3", signal_2);
         }
     }
 }
--- Assets/Shaders/PlottingShader.shader
+++ Assets/Shaders/PlottingShader.shader
@@ -3,10 +3,20 @@
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
-        _LineColour("Line Colour", Color) = (1,0,0,1)
+        _channel_1("Channel 1 Active",Range(0,1)) = 1.0
+        _LineColour_1("Line Colour (Channel 1)", Color) = (1,0,0,1)
+        _Offset_1("Offset", Range(-1,1)) = 0.0
+        _channel_2("Channel 2 Active",Range(0,1)) = 0.0
+        _LineColour_2("Line Colour (Channel 2)", Color) = (0,1,0,1)
+        _Offset_2("Offset", Range(-1,1)) = 0.0
+        _LineColour_3("Line Colour (Channel 2)", Color) = (0,0,1,1)
+        _channel_3("Channel 3 Active",Range(0,1)) = 0.0
+        _Offset_3("Offset", Range(-1,1)) = 0.0
         _LineWidth("Line Width", Range(0,2)) = 1.0
         _Scale("Scale", Range(0,2)) = 1.0
         _Offset("Offset", Range(-1,1)) = 0.0
+
+        _Speed("Speed", Range(1,10)) = 1
     }
         SubShader
         {
@@ -23,11 +33,27 @@
 
                 #include "UnityCG.cginc"
 
-                float _data[1000];
-                float4 _LineColour;
+                float _channel_1;
+                float _channel_2;
+                float _channel_3;
+
+                float _data_ch1[1000];
+                float _data_ch2[1000];
+                float _data_ch3[1000];
+                
+                float4 _LineColour_1;
+                float4 _LineColour_2;
+                float4 _LineColour_3;
+                
+                float _Offset_1;
+                float _Offset_2;
+                float _Offset_3;
+
                 float _LineWidth;
                 float _Scale;
                 float _Offset;
+
+                float _Speed;
             struct appdata
             {
                 float4 vertex : POSITION;
@@ -55,11 +81,22 @@
 
             fixed4 frag(v2f i) : SV_Target
             {
+            fixed4 col = float4(0.0,0.0,0.0,0.0);
                 // sample the texture
-                int j = (int)(i.uv.x * 1000);
-                float c = (_data[j]*_Scale) + _Offset;
-                fixed4 col = _LineColour * (float)(i.uv.y<=(c+_LineWidth)&&i.uv.y >= (c -_LineWidth));
-                
+            for (int j = 0; j < 3; j++) {
+                float data[1000];
+                for (int jj = 0; jj < 1000; jj++) {
+                    data[jj] = ((float)(_channel_1 > 0) * _data_ch1[jj]) * (float)(j==0);
+                    data[jj] += ((float)(_channel_2 > 0) * _data_ch2[jj]) * (float)(j == 1);
+                    data[jj] += ((float)(_channel_3 > 0) * _data_ch3[jj]) * (float)(j == 2);
+                }
+                int k = (int)((i.uv.x * 1000)/_Speed);
+                float offset = _Offset_1 * (float)(j == 0) + _Offset_2 * (float)(j == 1) + _Offset_3 * (float)(j == 2);
+                float c = (data[k] * _Scale) + offset + _Offset;
+                col += (float)(j == 0) * _LineColour_1 * (float)(i.uv.y <= (c + _LineWidth) && i.uv.y >= (c - _LineWidth)) * _channel_1;
+                col += (float)(j == 1) * _LineColour_2 * (float)(i.uv.y <= (c + _LineWidth) && i.uv.y >= (c - _LineWidth)) * _channel_2;
+                col += (float)(j == 2) * _LineColour_3 * (float)(i.uv.y <= (c + _LineWidth) && i.uv.y >= (c - _LineWidth)) * _channel_3;
+            }
                 // apply fog
                 UNITY_APPLY_FOG(i.fogCoord, col);
                 return col;
